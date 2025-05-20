4732f5fd9eb59758950c9a8ef43c8c239abaf355
NetcrackerCLPLCI <NetcrackerCLPLCI@users.noreply.github.com>
2025-05-20 08:34:59 +0000

    deploy: 8949f21122aa4fe85384f8b38c48ff41d6c600fa

---
+++ broadcast-common-files/graph/include-reference-appliers.d.ts
@@ -0,0 +1,4 @@
+import { DirtyStateModificationAppliersFactory } from './appliers-common';
+import { IncludePropertiesGroupRelationView } from './view-definition';
+
+export declare const includePropertiesEdgeDirtyStatusApplierFactory: DirtyStateModificationAppliersFactory<IncludePropertiesGroupRelationView>;
